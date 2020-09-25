baa067d4fb4928ec05d8e61a55a27540d465c337
Matheus Batistela <matheushenriquebatistela@gmail.com>
2020-09-25 14:28:31 -0300

    add test management

--- knn.py
+++ knn.py
@@ -7,33 +7,32 @@ from statistics import mode
 class KNeighbors:
     def __init__(self, n_neighbors=3):
         self.n_neighbors = n_neighbors
-        self.calc_distance = self.euclidian_distance
+        self.calc_distance = self.euclidean_distance
         self.training_dataframe = None
         
     
     @staticmethod
-    def euclidian_distance(x,y):
+    def euclidean_distance(x,y):
         return distance.euclidean(x,y)
     
     @staticmethod
-    def manhattam_distance(x,y):
+    def manhattan_distance(x,y):
         return distance.cityblock(x,y)
     
     def set_training_data(self, trainig_data):
         self.training_dataframe = trainig_data
         
     def set_distance_type(self,distance_type):
-        if distance_type == "euclidian":
-            self.calc_distance = self.euclidian_distance
-        elif distance_type == "manhattam":
-            self.calc_distance = self.manhattam_distance
+        if distance_type == "euclidean":
+            self.calc_distance = self.euclidean_distance
+        elif distance_type == "manhattan":
+            self.calc_distance = self.manhattan_distance
         else:
-            print("Wrong distance, using default (euclidian)")
+            print("Wrong distance, using default (euclidean)")
             
-    
     def classify(self,new_object):                
         
-        nearest_neighbors= [[float('inf'),None] for _ in range(3)]
+        nearest_neighbors= [[float('inf'),None] for _ in range(self.n_neighbors)]
 
         for _class, _object in self.training_dataframe.iterrows():
 
@@ -44,7 +43,8 @@ class KNeighbors:
                 nearest_neighbors[0][0] = neighbor_distance
                 nearest_neighbors[0][1] = _class
         
-        print(nearest_neighbors)
+        # print(nearest_neighbors)
         nearest_neighbors = [_class[1] for _class in nearest_neighbors]
         
+
         return mode(nearest_neighbors)    
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import pandas as pd
 import knn as knn
+import random
 
 def min_max(attribute):
     return (attribute - attribute.min())/(attribute.max() - attribute.min())
@@ -13,6 +14,35 @@ def normalize(data_matrix,normalization):
     df = pd.DataFrame(data_matrix,index=data_matrix[:,-1].astype(int))
     df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
     return df.apply(normalization , axis=1).copy()
+
+def result_file(instances):
+    output = open('result.txt','w')
+    for instance in instances:
+        output.write("-----------------------------------------------------------------------\n")
+        output.write(f"| K = {instance['k']} | Porcentagem: {instance['percent']*100} % | Normalização: {instance['normalization']} | Distância: {instance['distance']} |\n\n")
+        cf = np.asarray(instance['confusion_matrix'])
+        accuracy = (cf.trace()/cf.sum())*100
+        output.write(f"| Taxa de acerto: {accuracy} %, erro: {100 - accuracy} % |\n\n")
+        for _class in instance['confusion_matrix']:
+            output.write(f"{_class}\n")
+        output.write("\n")
+        output.write("-----------------------------------------------------------------------\n")
+        
+def reduce_dataframe(dataframe, percent):
+    if(percent == 1):
+        return dataframe
+    
+    dataframe_indexes = dataframe.index.values
+    dataframe_indexes = list(dict.fromkeys(dataframe_indexes))
+
+    class_data = []
+    for _index in dataframe_indexes:
+        class_data.append(
+            dataframe.groupby(
+            dataframe.index).get_group(_index).sample(
+            frac=percent,random_state=random.randrange(0,100)))
+
+    return pd.concat(class_data)
 
 if __name__ == "__main__":
     
@@ -26,18 +56,39 @@ if __name__ == "__main__":
     except OSError:
         print("File does not exist")
         
-   
-    training_norm_df = normalize(training_matrix,min_max)
-    test_norm_df = normalize(test_matrix,min_max)
+    instances = []
+        
+    for percent in (0.25,0.5,1):  
+          
+        for normalization in (min_max,z_score):
+            
+            training_norm_df = reduce_dataframe(normalize(training_matrix,normalization),percent)
+            test_norm_df = reduce_dataframe(normalize(test_matrix,normalization),percent)
+            
+            for distance in ("euclidean","manhattan"):
+                
+                for k in (1,3,5,7,9,11,13,15,17,19):
+                    
+                    _knn = knn.KNeighbors(n_neighbors=k)
+                    _knn.set_training_data(training_norm_df)
+                    _knn.set_distance_type(distance)   
+                    
+                    confusion_matrix = [ [ 0 for _ in range(10) ] for _ in range(10) ]
+                    
+                    for _class, _object in test_norm_df.iterrows():
+                        classified_class = _knn.classify(_object.values)
+                        confusion_matrix[_class][classified_class] += 1 
+                        
+                    instance = {
+                        "k": k,
+                        "percent": percent,
+                        "normalization": normalization.__name__,
+                        "distance": distance,
+                        "confusion_matrix": confusion_matrix
+                    }                    
+                    
+                    instances.append(instance)    
+              
     
-    _object = test_norm_df.iloc[0].values
-    
-    _knn = knn.KNeighbors(n_neighbors=3)
-    
-    _knn.set_distance_type("manhattam")
-    
-    _knn.set_training_data(training_norm_df)
-    
-    print(_knn.classify(_object))
-    
-    
+    result_file(instances)
+    print("Done !")
